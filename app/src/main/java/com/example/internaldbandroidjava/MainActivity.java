10dcfbdbde1edecd6877f732f443aa168ff809b9
Manfrin92 <manfredd_10@hotmail.com>
2021-10-12 08:31:51 -0300

    feat: ui to insert value

---
+++ app/src/main/java/com/example/internaldbandroidjava/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.internaldbandroidjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mInserir(View view) {
+
+    }
+}
